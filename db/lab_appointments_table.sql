e1e7fa5b2998ab16ad578ba7c56999eb1f0d37a2
Harsh Kumar <78796304+HarshaK47@users.noreply.github.com>
2021-04-30 17:47:18 +0530

    Update lab_appointments_table.sql

--- db/lab_appointments_table.sql
+++ db/lab_appointments_table.sql
@@ -1,17 +1,12 @@
 USE svasthya_pro;
 
-CREATE TABLE appointments(
-  userID varchar(30) NOT NULL,
-    testname varchar(50) NOT NULL,
-    cityname varchar(30) NOT NULL,
-    timings int NOT NULL,
-    lab varchar(30) NOT NULL,
-    day DATE NOT NULL,
-    id char(100) PRIMARY KEY  NOT NULL
+CREATE TABLE lab_appointments(
+	uniqueID varchar(90) PRIMARY KEY NOT NULL,
+	patient varchar(15) NOT NULL,
+	Lab_ID varchar(30) NOT NULL,
+	appt_date DATE NOT NULL,
+	appt_time INT NOT NULL
 );
 
-INSERT INTO appointments(userID,testname,cityname,timings,lab,day,id)
-   VALUES ('harsha','bloodtest','chd','1700','lalpath','2021-04-29','bloodtestchd1700lalpath2021-04-29');
 
-INSERT INTO appointments(userID,testname,cityname,timings,lab,day,id)
-  VALUES ('amber','sugar','chd','1700',' DR LAL PATHLABS','2021-04-29','sugarchd1700 DR LAL PATHLABS2021-04-29');
+
